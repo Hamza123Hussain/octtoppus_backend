388d30d1b98bcb84bf59e08ad5be32b15d9f81b8
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-17 14:11:00 +0500

    routes added for blog router

---
+++ DB/Router/BlogRouter.js
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+import { AddNewBlog } from '../Controllers/Blog/AddBlog.js'
+import { upload } from '../../multerconfig.js'
+import { GetAllBlogs } from '../Controllers/Blog/GettingAll.js'
+import { UpdateBlog } from '../Controllers/Blog/UpdateBlog.js'
+const BlogRouter = Router()
+BlogRouter.post('/UpdateBlog', UpdateBlog)
+BlogRouter.get('/GetAll', GetAllBlogs)
+BlogRouter.post('/AddBlog', upload.single('image'), AddNewBlog)
+// BlogRouter.get('/ALLPRODUCTS', GetAllProducts)
+export default BlogRouter
