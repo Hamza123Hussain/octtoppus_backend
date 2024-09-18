654852ef789707461bbceababce258950db8a724
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-18 14:23:22 +0500

    more to be done

--- DB/Router/BlogRouter.js
+++ DB/Router/BlogRouter.js
@@ -6,6 +6,7 @@ import { UpdateBlog } from '../Controllers/Blog/UpdateBlog.js'
 const BlogRouter = Router()
 BlogRouter.post('/UpdateBlog', UpdateBlog)
 BlogRouter.get('/GetAll', GetAllBlogs)
-BlogRouter.post('/AddBlog', upload.single('image'), AddNewBlog)
+BlogRouter.post('/AddBlog', upload.array('images', 5), AddNewBlog)
+// This will allow uploading up to 5 images at once
 
 export default BlogRouter
