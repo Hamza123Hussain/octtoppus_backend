e51e16e34b6db3214ee63ed11866536c41fd62c9
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-21 15:06:34 +0500

    deletion and fetching functions have worked

--- DB/Controllers/Blog/DeleteBlog.js
+++ DB/Controllers/Blog/DeleteBlog.js
@@ -1,12 +1,13 @@
+import { deleteDoc, doc } from 'firebase/firestore'
 import { DB } from '../../../FireBaseConfig.js'
 
 export const DeleteBlog = async (req, res) => {
   const { id } = req.query
 
   try {
-    const blogRef = DB.collection('Blogs').doc(id)
-    await blogRef.delete()
-    res.status(200).json(true)
+    await deleteDoc(doc(DB, 'Blogs', id))
+
+    res.status(200).json({ success: true })
   } catch (error) {
     console.error('Error deleting blog:', error)
     res.status(500).json({ message: 'Error deleting blog' })
--- DB/Controllers/Blog/GetSingle.js
+++ DB/Controllers/Blog/GetSingle.js
@@ -1,13 +1,14 @@
+import { doc, getDoc } from 'firebase/firestore'
 import { DB } from '../../../FireBaseConfig.js'
 
 export const GetSingleBlog = async (req, res) => {
   const { id } = req.query
 
   try {
-    const blogRef = DB.collection('Blogs').doc(id)
-    const blogDoc = await blogRef.get()
+    const blogRef = doc(DB, 'Blogs', id) // Get a reference to the document
+    const blogDoc = await getDoc(blogRef) // Fetch the document
 
-    if (!blogDoc.exists) {
+    if (!blogDoc.exists()) {
       return res.status(404).json({ message: 'Blog not found' })
     }
 
