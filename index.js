a7ac8ab43e912db7739e1ad97ddce79d36f607fd
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2026-01-31 15:12:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,9 @@
 import express from 'express'
 import cors from 'cors'
 import { Port1 } from './Config.js'
-import AuthRouter from './DB/Router/AuthRouter.js'
-import BlogRouter from './DB/Router/BlogRouter.js'
-import { blogsCollection } from './db.js'
 import Contactrouter from './DB/Router/ContactRouter.js'
 import { DB_CONNECTED } from './DBCoonect.js'
+import router from './DB/Router/BlogRouter.js'
 const app = express()
 
 // ------------------- MIDDLEWARE -------------------
@@ -20,58 +18,8 @@ app.use(express.json()) // Parse JSON request bodies
 
 // ------------------- ROUTES -------------------
 
-// Mount your existing routers
-app.use('/api/auth', AuthRouter)
-app.use('/api/blogs', BlogRouter)
 app.use('/api/contact', Contactrouter)
-// GET paginated list of blogs (metadata only)
-app.get('/api/blogs', async (req, res) => {
-  const page = parseInt(req.query.page) || 1 // default page 1
-  const limit = parseInt(req.query.limit) || 10 // default 10 blogs per page
-  const skip = (page - 1) * limit
-
-  try {
-    const blogs = await blogsCollection
-      .find(
-        {},
-        {
-          projection: {
-            title: 1,
-            image: 1,
-            titleLink: 1,
-            description: 1,
-
-            date: 1,
-            content: 1,
-          }, // only required fields
-        },
-      )
-      .sort({ date: -1 }) // sort by date descending
-      .skip(skip)
-      .limit(limit)
-      .toArray()
-
-    res.status(200).json(blogs)
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ success: false, message: 'Server error' })
-  }
-})
-
-// GET single blog by ID
-app.get('/api/singleblog', async (req, res) => {
-  try {
-    const blog = await blogsCollection.findOne({
-      titleLink: req.query.titleLink,
-    })
-    if (!blog)
-      return res.status(404).json({ success: false, message: 'Blog not found' })
-    res.status(200).json(blog)
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ success: false, message: 'Server Error' })
-  }
-})
+app.use('/api/blogs', router)
 
 // ------------------- START SERVER -------------------
 app.listen(Port1, () => {
