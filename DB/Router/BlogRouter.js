cc1b2a8afa2691db7dda00d66314b445cc0f12bb
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2026-01-31 15:12:40 +0500

    Update BlogRouter.js

--- DB/Router/BlogRouter.js
+++ DB/Router/BlogRouter.js
@@ -1,35 +1,18 @@
-import { Router } from 'express'
-import { AddNewBlog } from '../Controllers/Blog/AddBlog.js'
-import { upload } from '../../multerconfig.js'
-import { GetAllBlogs } from '../Controllers/Blog/GettingAll.js'
-import { UpdateBlog } from '../Controllers/Blog/UpdateBlog.js'
-import { GetSingleBlog } from '../Controllers/Blog/GetSingle.js'
-import { DeleteBlog } from '../Controllers/Blog/DeleteBlog.js'
-import { uploadVideo } from '../Controllers/Blog/UploadVideo.js'
+import express from 'express'
+import {
+  createBlog,
+  deleteBlog,
+  getBlogs,
+  getSingleBlog,
+  updateBlog,
+} from '../Controllers/Blog/AllInController.js'
 
-const BlogRouter = Router()
+const router = express.Router()
 
-// Route for updating an existing blog
-BlogRouter.post('/UpdateBlog', UpdateBlog)
+router.get('/', getBlogs)
+router.get('/single', getSingleBlog)
+router.post('/', createBlog)
+router.put('/updateblog', updateBlog)
+router.delete('/deleteblog', deleteBlog)
 
-// Route to get all blogs
-BlogRouter.get('/GetAll', GetAllBlogs)
-
-// Route to add a new blog post with header and section images
-BlogRouter.post(
-  '/AddBlog',
-  upload.fields([
-    { name: 'headerImage', maxCount: 1 }, // Allow 1 header image
-    ...Array.from({ length: 10 }, (_, i) => ({
-      name: `sectionImage_${i}`,
-      maxCount: 1, // Allow 1 section image per section
-    })),
-  ]),
-  AddNewBlog
-)
-
-BlogRouter.get('/GetSingle', GetSingleBlog)
-BlogRouter.delete('/DeleteBlog', DeleteBlog)
-BlogRouter.post('/UploadImage', upload.single('video'), uploadVideo)
-
-export default BlogRouter
+export default router
